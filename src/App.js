ff84da158bf8af5ce36068820405dd7976bca16c
valzanialdo@gmail.com <LA-TUA-EMAIL>
2024-09-06 17:22:11 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       <header>
         <Sidebar />
       </header>
-      <main>
+      {/* <main>
         <MusicSection  artistName='Queen'/>
-      </main>
+      </main> */}
       <footer>
         <PlayerControls />
       </footer>
